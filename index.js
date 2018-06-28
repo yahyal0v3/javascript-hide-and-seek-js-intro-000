70b9eae1d93f0c8a350ff128c8b487ae0b556003
Khaya Ramcharita-Dillon <yahyal0v3@yahoo.com>
2018-06-28 18:23:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,3 +1,15 @@
 function getFirstSelector(selector) {
-  document.querySelector(`${selector}`)
+  return document.querySelector(selector)
+}
+
+function nestedTarget() {
+  return document.querySelector('#nested div.target')
+}
+
+function deepestChild() {
+  return document.getElementById('grand-node').querySelectorAll('div')[3]
+}
+
+function increaseRankBy(n) {
+  return document.querySelectorAll('ul.ranked-list li').forEach(li => (li.innerHTML + 2).toString())
 }
